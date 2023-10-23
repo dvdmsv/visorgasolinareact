6415ff506baadca9b034405f7bf902071654fd33
dvdmsv <davidmarcelino80@gmail.com>
2023-10-23 11:40:51 +0200

    ~Ahora la web se inicia mostrando Gasoleo A directamente

--- src/App.js
+++ src/App.js
@@ -4,23 +4,35 @@ import ParrafoCombustible from './componentes/ParrafoCombustible/ParrafoCombusti
 import Selectores from './componentes/Selectores/Selectores';
 import Tabla from './componentes/Tabla/Tabla';
 import { IdsProvider } from './servicios/IDsContext.js';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [parametroCombustible, setParametroCombustible] = useState(null);
 
-  const urlParam = new URLSearchParams(window.location.search);
-  const parametro = urlParam.get("combustible");
+  useEffect(() => {
+    // Obtén el valor actual del parámetro 'combustible' de la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const parametroCombustible = urlParams.get('combustible');
+    //Se actualiza el estado del combustible
+    setParametroCombustible(parametroCombustible);
 
+    // Si 'combustible' no está presente redirige a la URL deseada
+    if (!parametroCombustible) {
+        // Redirige a la URL con el parámetro deseado
+        window.location.href = `${window.location.origin}${window.location.pathname}?combustible=Precio%20Gasoleo%20A`;
+    }
+  }, []);
 
   return (
     <div className="App">
       <IdsProvider>
         <BarraNavegacion />
         <ParrafoCombustible 
-          combustible={parametro}
+          combustible={parametroCombustible}
         />
         <Selectores />
         <Tabla
-          combustibleURL={parametro}
+          combustibleURL={parametroCombustible}
         />
       </IdsProvider>
     </div>
